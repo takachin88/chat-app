1f810fec5530289f1287e1fbe0da888b5cd7d440
takachin88 <mototakato@icloud.com>
2021-06-28 19:40:29 +0900

    チャット画面のフロント実装

--- config/application.rb
+++ config/application.rb
@@ -1,8 +1,21 @@
-# 省略
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
 module ChatApp
   class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-   # 中略
+    config.i18n.default_locale = :ja
+    config.time_zone = 'Tokyo'
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
     config.generators do |g|
       g.stylesheets false
       g.javascripts false
@@ -11,4 +24,3 @@ module ChatApp
     end
   end
 end
-
